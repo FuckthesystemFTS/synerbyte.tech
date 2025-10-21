a44d65c57acf81cd689f6a3935890d509b9827f7
Your Name <charlieracconta@example.com>
2025-10-21 21:30:04 +0200

    Hide download buttons temporarily

---
+++ commit-and-push.bat
@@ -0,0 +1,3 @@
+@echo off
+git commit -m "Fix TypeScript errors for Heroku deploy"
+git push heroku master
---
+++ quick-deploy.bat
@@ -0,0 +1,6 @@
+@echo off
+cd C:\Users\Hp\Desktop\SYNERCHAT
+git add .
+git commit -m "Hide download buttons temporarily"
+git push heroku master
+pause
